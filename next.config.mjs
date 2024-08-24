b33b16bbb3ee3377da4f3ec7fc9d1cca4a041cd8
Dung Le <xdung24@gmail.com>
2024-08-24 12:22:47 +0700

    Export static

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   async redirects() {
     return [
